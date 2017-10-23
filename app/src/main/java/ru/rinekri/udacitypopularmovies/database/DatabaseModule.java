5d348d3d2741fb55ffc5e569e0c51caf5fdccdf2
ilya <rinekri@gmail.com>
2017-10-23 13:21:55 +0200

    Add escaped annotations for the DatabaseModule

--- app/src/main/java/ru/rinekri/udacitypopularmovies/database/DatabaseModule.java
+++ app/src/main/java/ru/rinekri/udacitypopularmovies/database/DatabaseModule.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import dagger.Module;
+import dagger.Provides;
+import ru.rinekri.udacitypopularmovies.annotations.ApplicationScope;
 
 @Module
 public class DatabaseModule {
+  @Provides
+  @ApplicationScope
   public SQLiteOpenHelper provideDatabaseHelper(Context context) {
     return new DatabaseHelper(context);
   }
